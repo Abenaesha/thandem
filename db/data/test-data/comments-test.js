5ef1b30281be51833ad18fc2c90b5710602e66d9
Togden <tom.ogden12@outlook.com>
2021-04-20 10:11:32 +0100

    add comments test data

---
+++ db/data/test-data/comments-test.js
@@ -0,0 +1,49 @@
+/*
+comments_id - - - auto generated
+created_at - - - auto generated
+*/
+
+module.exports = [
+  {
+    author: "Hannah123",
+    ride_id: 1,
+    votes: 10,
+    body: "would love to join but busy this day, maybe next time!",
+  },
+  {
+    author: "t0gden",
+    ride_id: 1,
+    votes: 5,
+    body: "count me in!",
+  },
+  {
+    author: "NadiaPedals",
+    ride_id: 2,
+    votes: 11,
+    body: "wouldnt miss it for the world :-)",
+  },
+  {
+    author: "RollingDan",
+    ride_id: 3,
+    votes: 30,
+    body: "Sounds great!",
+  },
+  {
+    author: "RaofRides",
+    ride_id: 1,
+    votes: 3,
+    body: "cant make it this time im afraid as i am tutoring :-)",
+  },
+  {
+    author: "t0gden",
+    ride_id: 2,
+    votes: 1,
+    body: "I'll come but hope i am fit enough!!",
+  },
+  {
+    author: "Hannah123",
+    ride_id: 3,
+    votes: 26,
+    body: "sounds lovely. count me in",
+  },
+]

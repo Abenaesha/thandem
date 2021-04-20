93919719c6a4603e3fe6451e2c1ab2921faf5712
Togden <tom.ogden12@outlook.com>
2021-04-20 09:30:43 +0100

    add user test data

--- db/data/test-data/users-test.js
+++ db/data/test-data/users-test.js
@@ -1,42 +1,53 @@
+/* user id will be auto generated / assigned
+username / password / email - may not be require as we will use strava auth (maybe - google/facebook also) BUT is included here for example purposes
+routes_data - will be brought in by strava api
+created_at will be auto generated
+*/
+
 module.exports = [
-	{
-		username: "t0gden",
-		firstName: "Tom",
-		lastName: "Ogden",
-		email: "example1@gmail.com",
-		password: "abcde1",
-		avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
-	},
-	{
-		username: "RaofRides",
-		firstName: "Raof",
-		lastName: "Benaesha",
-		email: "example2@gmail.com",
-		password: "abcde2",
-		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-	},
-	{
-		username: "RollingDan",
-		firstName: "Daniel",
-		lastName: "Stevenson",
-		email: "example3@gmail.com",
-		password: "abcde3",
-		avatar_url: "http://clipart-library.com/images/pc78gEXRi.jpg",
-	},
-	{
-		username: "Hannah123",
-		firstName: "Hannah",
-		lastName: "Lacey",
-		email: "example4@gmail.com",
-		password: "abcde4",
-		avatar_url: "http://clipart-library.com/images/6Tr6RExRc.png",
-	},
-	{
-		username: "NadiaPedals",
-		firstName: "Nadia",
-		lastName: "Amir",
-		email: "example5@gmail.com",
-		password: "abcde5",
-		avatar_url: "http://clipart-library.com/images/KLcdBGgT4.jpg",
-	},
+  {
+    username: "t0gden",
+    first_name: "Tom",
+    last_name: "Ogden",
+    email: "example1@gmail.com",
+    password: "abcde1",
+    avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
+    location: "Chester",
+  },
+  {
+    username: "RaofRides",
+    first_name: "Raof",
+    last_name: "Benaesha",
+    email: "example2@gmail.com",
+    password: "abcde2",
+    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+    location: "Manchester",
+  },
+  {
+    username: "RollingDan",
+    first_name: "Daniel",
+    last_name: "Stevenson",
+    email: "example3@gmail.com",
+    password: "abcde3",
+    avatar_url: "http://clipart-library.com/images/pc78gEXRi.jpg",
+    location: "Sheffield",
+  },
+  {
+    username: "Hannah123",
+    first_name: "Hannah",
+    last_name: "Lacey",
+    email: "example4@gmail.com",
+    password: "abcde4",
+    avatar_url: "http://clipart-library.com/images/6Tr6RExRc.png",
+    location: "Manchester",
+  },
+  {
+    username: "NadiaPedals",
+    first_name: "Nadia",
+    last_name: "Amir",
+    email: "example5@gmail.com",
+    password: "abcde5",
+    avatar_url: "http://clipart-library.com/images/KLcdBGgT4.jpg",
+    location: "London",
+  },
 ]

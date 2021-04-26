57479b8d908b6b43e88dcc1ec24034580027f0ca
abenaesha <abenaesha@github.com>
2021-04-26 14:07:18 +0100

    adjust connection variables

--- db/connection.js
+++ db/connection.js
@@ -15,11 +15,11 @@ const dbConfig =
   	? { 
   		client: "pg", 
   		connection: {
-  			user: process.env.USER,
-  			host: process.env.HOST,
-  			database: process.env.DATABASE,
-  			password: process.env.PASSWORD,
-  			port: process.env.PORT,
+  			user: process.env.DB_USER,
+  			host: process.env.DB_HOST,
+  			database: process.env.DB_DATABASE,
+  			password: process.env.DB_PASSWORD,
+  			port: process.env.DB_PORT,
   		} 
   	}
   	: require( "../knexfile" )

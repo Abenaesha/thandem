310e8f26b2280bec74056a4cdbb1dbfd07b6d917
abenaesha <abenaesha@github.com>
2021-04-26 12:28:07 +0100

    add production env to connection

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,26 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+// const knex = require("knex")
+// const dbConfig = require( "../knexfile" )
+
+// const connection = knex(dbConfig)
+
+// module.exports = connection
+
+const ENV = process.env.NODE_ENV || "development"
 const knex = require("knex")
-const dbConfig = require( "../knexfile" )
 
-const connection = knex(dbConfig)
-
-module.exports = connection
+const dbConfig =
+  ENV === "production"
+  	? { 
+  		client: "pg", 
+  		connection: { 
+  			connectionString: process.env.DATABASE_URL,
+  			ssl: {
+  				rejectUnauthorized: false,
+  			},
+  		}, 
+  	}
+  	: require( "../knexfile" )
+    
+module.exports = knex(dbConfig)

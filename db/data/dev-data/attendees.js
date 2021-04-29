3844da258c4774beea29f6ebceb32a4a2825eb6d
Togden <tom.ogden12@outlook.com>
2021-04-29 17:14:39 +0100

    update seed data

--- db/data/dev-data/attendees.js
+++ db/data/dev-data/attendees.js
@@ -1,50 +1,50 @@
 module.exports = [
-  {
-    attendee: "t0gden",
-    ride_id: 1,
-    name: "Tom",
-    avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
-  },
-  {
-    attendee: "t0gden",
-    ride_id: 3,
-    name: "Tom",
-    avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
-  },
-  {
-    attendee: "rollingDan",
-    ride_id: 2,
-    name: "Dan",
-    avatar_url: "http://clipart-library.com/images/pc78gEXRi.jpg",
-  },
-  {
-    attendee: "raofRides",
-    ride_id: 1,
-    name: "Raof",
-    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-  },
-  {
-    attendee: "raofRides",
-    ride_id: 3,
-    name: "Raof",
-    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-  },
-  {
-    attendee: "singleSyllableDave",
-    ride_id: 3,
-    name: "SingleSyllableDave",
-    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-  },
-  {
-    attendee: "vel-odrome",
-    ride_id: 3,
-    name: "VEL-odrome",
-    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-  },
-  {
-    attendee: "doubleSyllableDavid",
-    ride_id: 3,
-    name: "doubleSyllableDavid",
-    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
-  },
+	{
+		attendee: "t0gden",
+		ride_id: 1,
+		name: "Tom",
+		avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
+	},
+	{
+		attendee: "t0gden",
+		ride_id: 3,
+		name: "Tom",
+		avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
+	},
+	{
+		attendee: "rollingDan",
+		ride_id: 2,
+		name: "Dan",
+		avatar_url: "http://clipart-library.com/images/pc78gEXRi.jpg",
+	},
+	{
+		attendee: "raofRides",
+		ride_id: 1,
+		name: "Raof",
+		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+	},
+	{
+		attendee: "raofRides",
+		ride_id: 3,
+		name: "Raof",
+		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+	},
+	{
+		attendee: "singleSyllableDave",
+		ride_id: 3,
+		name: "SingleSyllableDave",
+		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+	},
+	{
+		attendee: "vel-odrome",
+		ride_id: 3,
+		name: "VEL-odrome",
+		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+	},
+	{
+		attendee: "doubleSyllableDavid",
+		ride_id: 3,
+		name: "doubleSyllableDavid",
+		avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+	},
 ]

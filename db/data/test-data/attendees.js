7b15971bc2dd2e5b02bd2816e6991f41fa2947ed
Togden <tom.ogden12@outlook.com>
2021-04-29 09:45:04 +0100

    add avatar_url to attendees table that defaults to the users table avatar_url

--- db/data/test-data/attendees.js
+++ db/data/test-data/attendees.js
@@ -1,12 +1,14 @@
 module.exports = [
-	{
-		attendee: "t0gden",
-		ride_id: 1,
-		name: "Tom",
-	},
-	{
-		attendee: "raofRides",
-		ride_id: 1,
-		name: "Raof",
-	},
+  {
+    attendee: "t0gden",
+    ride_id: 1,
+    name: "Tom",
+    avatar_url: "http://clipart-library.com/images/8TEjdRMEc.png",
+  },
+  {
+    attendee: "raofRides",
+    ride_id: 1,
+    name: "Raof",
+    avatar_url: "http://clipart-library.com/images/yckAgeMRi.jpg",
+  },
 ]

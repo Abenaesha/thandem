dfdbbba6976b5ecfee0db74075d00531fe7686c9
Togden <tom.ogden12@outlook.com>
2021-04-29 12:22:22 +0100

    update char limit to 8000 in rides table

--- db/migrations/20210419090638_create-rides-table.js
+++ db/migrations/20210419090638_create-rides-table.js
@@ -1,20 +1,20 @@
-exports.up = function(knex) {
-	return knex.schema.createTable( "rides", ( rideTable ) => {
-		rideTable.increments( "ride_id" ).primary()
-		rideTable.string( "author" ).references( "users.username" ).onDelete( "CASCADE" )
-		rideTable.datetime("ride_date").notNullable()
-		rideTable.string( "route_data" ).notNullable()
-		rideTable.string( "ride_type" ).notNullable()
-		rideTable.string( "title" ).notNullable()
-		rideTable.string("description").notNullable()
-		rideTable.string( "experience_level" ).notNullable()
-		rideTable.integer( "joins" ).defaultTo( 0 )
-		rideTable.string("location").notNullable()
-		rideTable.timestamp( "created_at" ).defaultTo( knex.fn.now() )
-		rideTable.string("distanceInKm").notNullable()
-	})
+exports.up = function (knex) {
+  return knex.schema.createTable("rides", (rideTable) => {
+    rideTable.increments("ride_id").primary()
+    rideTable.string("author").references("users.username").onDelete("CASCADE")
+    rideTable.datetime("ride_date").notNullable()
+    rideTable.string("route_data", 8000).notNullable()
+    rideTable.string("ride_type").notNullable()
+    rideTable.string("title").notNullable()
+    rideTable.string("description").notNullable()
+    rideTable.string("experience_level").notNullable()
+    rideTable.integer("joins").defaultTo(0)
+    rideTable.string("location").notNullable()
+    rideTable.timestamp("created_at").defaultTo(knex.fn.now())
+    rideTable.string("distanceInKm").notNullable()
+  })
 }
 
-exports.down = function(knex) {
-	return knex.schema.dropTable("rides")
+exports.down = function (knex) {
+  return knex.schema.dropTable("rides")
 }
--- db/migrations/20210425114754_create-attendees-table.js
+++ db/migrations/20210425114754_create-attendees-table.js
@@ -1,19 +1,19 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("attendees", (attendeesTable) => {
-    attendeesTable.increments("attendee_id").primary()
-    attendeesTable
-      .string("attendee")
-      .references("users.username")
-      .onDelete("CASCADE")
-    attendeesTable
-      .integer("ride_id")
-      .references("rides.ride_id")
-      .onDelete("CASCADE")
-    attendeesTable.string("name").notNullable()
-    attendeesTable.string("avatar_url")
-  })
+	return knex.schema.createTable("attendees", (attendeesTable) => {
+		attendeesTable.increments("attendee_id").primary()
+		attendeesTable
+			.string("attendee")
+			.references("users.username")
+			.onDelete("CASCADE")
+		attendeesTable
+			.integer("ride_id")
+			.references("rides.ride_id")
+			.onDelete("CASCADE")
+		attendeesTable.string("name").notNullable()
+		attendeesTable.string("avatar_url")
+	})
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("attendees")
+	return knex.schema.dropTable("attendees")
 }
